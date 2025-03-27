20b1ee13caaedd52ccd4ee66f068a489853bca7d
LaraHo <laraho0202@gmail.com>
2025-03-26 21:07:37 -0700

    basic structure

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
